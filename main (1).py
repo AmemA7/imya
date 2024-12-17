2861fd644a204e6c389cd096df96c92be5850ade
AmemA7 <gamer71539@gmail.com>
2024-12-17 15:41:06 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,12 @@
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+from window import Window
+
+import sys
+
+
+if __name__ == "__main__":
+    app: QApplication = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
